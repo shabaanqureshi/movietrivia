158d6a4173294202e9107e21155fa2fe5c6d2246
Shabaan Qureshi <shabaanqureshi>
2018-10-25 22:23:54 -0700

    Progress on the game

--- src/components/movietrivia/MovieTrivia.js
+++ src/components/movietrivia/MovieTrivia.js
@@ -1,12 +1,73 @@
 import React from 'react';
-import HomePage from '../home/HomePage';
+import PropTypes from 'prop-types';
+import '../../styles/MovieTrivia.css';
 import '../../../src/bootstrap.min.css'; 
 
+function ScoreDisplay() {
+  return (
+    <h1 className = "score"> SCORE </h1> 
+  )
+}
 
-function MovieTrivia({onContinue}) {
+function ScoreBoard() {
+  return (
+  <div class="container">
+  <div class="row board">
+    <div class="col">Computer</div>
+    <div class="col">You</div>
+    <div class="w-100"></div>
+    <div class="col">Computer</div>
+    <div class="col">You</div>
+  </div>
+  </div>)
+  ;
+}
+
+function Movie({name, onClick}) {
+  return (
+    <div className = "list" onClick = {() => {onClick(name);}}>
+    <h4>{name}</h4> 
+    </div>
+  );
+}
+
+function TextToInk(ink) {
+  const mapping = {
+    'none': '',
+    'correct': 'green',
+    'wrong': 'red'
+  };
+  return mapping[ink];
+}
+
+function Data({actor, movies, ink, onAnswerSelected}) {
+  return (
+  <div className="row data">
+    <div className = "col-5 offset-1">
+      <img src={actor.imageUrl} className = "actorimage" alt="Actor"/>
+    </div>
+    <div className = "col-5" style={{color: TextToInk(ink)}}>
+      {movies.map(name => <Movie name  = {name} key = {name} onClick = {onAnswerSelected} /> )};
+    </div>
+  </div>);
+}
+Data.propTypes = {
+  actor: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    movies: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  movies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  ink: PropTypes.string.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+};
+
+function MovieTrivia({data, ink, onAnswerSelected}) {
   return (
     <div className="container-fluid">
-      <HomePage onContinue={onContinue}/>
+    <ScoreDisplay />
+    <ScoreBoard />
+    <Data {...data} ink = {ink} onAnswerSelected = {onAnswerSelected}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import {Route, BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 import HomePage from './components/home/HomePage';
 import MovieTrivia from '../src/components/movietrivia/MovieTrivia';
 import ActorForm from './components/actorform/ActorForm';
@@ -45,17 +45,83 @@ const actors = [
     }
 ];
 
+/*
+Helper function to randomize the ordering of our movies array
+*/
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+/*
+Helper function to choose a random value
+*/
+function sample(items)
+{
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+/*
+Function to return all the movies from all actors into one array
+*/
+function allMovies(actors) {
+    return actors.reduce((p, c, i) => {
+        return p.concat(c.movies);
+    }, []);
+};
+
+/*
+Function to generate the choices of movies and the actor
+*/
+function getData(actors) {
+    const movies = allMovies(actors);
+    const fiveRandomMovies = shuffle(movies).slice(0,6);
+    const answer = sample(fiveRandomMovies);
+    return {
+        movies: fiveRandomMovies,
+        actor: actors.find(actor =>  actor.movies.some((title => title === answer)))
+    }
+}
+
+const state = {
+    data: getData(actors),
+    ink: ''
+};
+
+/*
+Function to determine which color to color the text depending if the user's answer is correct or wrong
+*/
+function onAnswerSelected(answer) {
+    const choice = state.data.actor.movies.some(movie => movie === answer);
+    if (choice) {
+        state.ink = 'correct';
+    }
+    else {
+        state.ink = 'wrong';
+    }
+    render();
+  }
+
+function Game() {
+    return<MovieTrivia {...state} onAnswerSelected = {onAnswerSelected}/>
+}
+
 function render() {
-    ReactDOM.render(
-    <BrowserRouter>
+  ReactDOM.render(
+      <BrowserRouter>
       <React.Fragment>
-        <Route exact path = "/" component = {HomePage} />
-        <Route exact path = "/game" component = {MovieTrivia} />
-        <Route exact path= "/add" component = {ActorForm} />
+      <Route exact path = "/" component = {HomePage} />
+      <Route path = "/game" component = {Game} />
+      <Route path = "/add" component = {ActorForm} />
       </React.Fragment>
     </BrowserRouter>, document.getElementById('root'));
-  }
-  render();
-
-
+}
+render();
 serviceWorker.unregister();
